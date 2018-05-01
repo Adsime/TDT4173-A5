68e597a2c252204079113ea76915093afbdb4218
adsime <adrianmelsom@gmail.com>
2018-05-01 17:54:10 +0200

    Added smoothing method to image class

--- image.py
+++ image.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as m
 
 class Image():
 
@@ -7,3 +8,14 @@ class Image():
 
     def print_data(self):
         print(self.image)
+
+    def get_image_map(self):
+        return self.image
+
+    def smooth(self):
+        new_img = np.zeros((20, 20))
+        n = 5
+        for x, row in enumerate(self.image):
+            for y, elem in enumerate(row):
+                new_img[x][y] = 0 if elem < 75 else 255 if elem > 170 else 125
+        return new_img
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from alphabet import Alphabet
+import matplotlib.pyplot as plt
 
 alphabet = Alphabet()
 
 
-for x in alphabet.get_images('b'):
-    x.print_data()
+imgs = alphabet.get_images('b')
+img = imgs[114]
+plt.imshow(img.get_image_map(), cmap="gray")
+plt.figure()
+
+
+plt.imshow(img.smooth(), cmap="gray")
+plt.show()
