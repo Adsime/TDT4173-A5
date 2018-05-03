7e534db4f888f3fc327db5504fead008035fcd94
adsime <adrianmelsom@gmail.com>
2018-05-03 20:00:13 +0200

    Added image detection file reader

--- temp_main.py
+++ temp_main.py
@@ -2,22 +2,27 @@ from imagehandler import ImageHandler
 import extractor as e
 from k_nn import KNN
 from svm import SVM
+import filehandler
 
-alphabet = ImageHandler()
+def test():
+    alphabet = ImageHandler()
 
-train_images, train_targets = alphabet.get_all_train_data()
-test_images, test_targets = alphabet.get_all_test_data()
+    train_images, train_targets = alphabet.get_all_train_data()
+    test_images, test_targets = alphabet.get_all_test_data()
 
-methods = [e.apply_local_threshold]
+    methods = [e.apply_local_threshold]
 
-train_images = e.extract(methods, train_images, True, True, 30)
-test_images = e.extract(methods, test_images, True)
+    train_images = e.extract(methods, train_images, True, True, 40)
+    test_images = e.extract(methods, test_images, True)
 
-knn = KNN()
-svm = SVM()
+    knn = KNN()
+    svm = SVM()
 
-knn.train(train_images, train_targets)
-svm.train(train_images, train_targets)
+    knn.train(train_images, train_targets)
+    svm.train(train_images, train_targets)
 
-print(knn.score(test_images, test_targets))
-print(svm.score(test_images, test_targets))
+    print(knn.score(test_images, test_targets))
+    print(svm.score(test_images, test_targets))
+
+
+print(filehandler.read_detection_image(1))
